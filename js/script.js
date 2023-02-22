8012bf7ace65c07ecd226a6de88854c7ef0efe6d
KlaudijaDemchyk <fkst@i.ua>
2023-02-22 15:27:57 +0100

    Init3

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
+var el = document.getElementById('main');
+var img = el.nameFile;
+img.addEventListener("change",function(e){
+    let selectedFile = img.files[0];
+    let fileUrl = URL.createObjectURL(selectedFile);
+    img.parentElement.insertAdjacentHTML(
+        "beforeend",
+        ` <div class ="main-form__image">
+             <img alt="" title="${selectedFile.name}" src ="${fileUrl}">
+        </div>`
+    );
+});
 document.getElementById('main').addEventListener("submit",checkForm);
 function checkForm(event){
     event.preventDefault();
-    var el = document.getElementById('main');
     var name = el.nameInput.value;
     var komment = el.nameTextarea.value;
     var rate = el.nameRadio.value;
-    var img = el.nameFile;
     var nameOwner = el.nameInputOwner.value;
     var age = el.nameSelect;
     var ageSelect = age.selectedIndex;
@@ -13,16 +23,6 @@ function checkForm(event){
     var agree = el.nameCheck;
     var date = el.inputDate.value;
     var fail ="";
-    img.addEventListener("change",function(e){
-     let selectedFile = img.files[0];
-     let fileUrl = URL.createObjectURL(selectedFile);
-     img.parentElement.insertAdjacentHTML(
-         "beforeend",
-        ` <div class ="main-form__image">
-             <img alt="" title="${selectedFile.name}" src ="${fileUrl}">
-        </div>`
-   );
-      });
     if ( name == "" || komment == "" || nameOwner == "" || agree.checked != true || date == "")
         fail = "Please, fill all filds";
     else if(name.length <= 1 || name.length > 20 || nameOwner.length <= 1 || nameOwner.length > 20)
@@ -31,5 +31,6 @@ function checkForm(event){
         document.getElementById('error').innerHTML = fail;
     else {
         alert ("All data is correctly filled in!\nName Diamond: "+name+"\nComment: "+komment+"\nImage"+img.value +"\nYour name: "+nameOwner+"\nAge: "+ageValue+"\nDate: "+date);
+        window.location.reload();
     }
 }
