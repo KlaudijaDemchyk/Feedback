c35c2640b6baa26b3a58427846d933c5b7cf22d4
KlaudijaDemchyk <fkst@i.ua>
2023-02-22 14:08:23 +0100

    Init

---
+++ js/script.js
@@ -0,0 +1,35 @@
+document.getElementById('main').addEventListener("submit",checkForm);
+function checkForm(event){
+    event.preventDefault();
+    var el = document.getElementById('main');
+    var name = el.nameInput.value;
+    var komment = el.nameTextarea.value;
+    var rate = el.nameRadio.value;
+    var img = el.nameFile.value;
+    var nameOwner = el.nameInputOwner.value;
+    var age = el.nameSelect;
+    var ageSelect = age.selectedIndex;
+    var ageValue = age.options[ageSelect].text;
+    var agree = el.nameCheck;
+    var date = el.inputDate.value;
+    var fail ="";
+    //img.addEventListener("change",function (e) {
+    // let selectedFile = img.files[0];
+     //let fileUrl = URL.createObjectURL(selectedFile);
+     //img.parentElement.insertAdjacentHTML(
+       //  "beforeend",
+      //   ` <div class ="main-form__image">
+      ///       <img alt="" title= "${fileUrl}">
+      //   </div>`
+  //   );
+ //   });
+    if ( name == "" || komment == "" || nameOwner == "" || agree.checked != true || date == "")
+        fail = "Please, fill all filds";
+    else if(name.length <= 1 || name.length > 20 || nameOwner.length <= 1 || nameOwner.length > 20)
+        fail = "Please enter correct name";
+    if (fail != "")
+        document.getElementById('error').innerHTML = fail;
+    else {
+        alert ("All data is correctly filled in!\n Name Diamond: "+name+"\n Komment: "+komment+"\n Your name: "+nameOwner+"\n Age: "+ageValue+"\n Date: "+date);
+    }
+}
