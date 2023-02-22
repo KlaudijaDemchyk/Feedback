eda329de4aa427ae7831fa33b14080f1d36c4681
KlaudijaDemchyk <fkst@i.ua>
2023-02-22 15:44:22 +0100

    Init5

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ document.getElementById('main').addEventListener("submit",checkForm);
 function checkForm(event){
     event.preventDefault();
     var name = el.nameInput.value;
-    var komment = el.nameTextarea.value;
+    var comment = el.nameTextarea.value;
     var nameOwner = el.nameInputOwner.value;
     var age = el.nameSelect;
     var ageSelect = age.selectedIndex;
@@ -22,14 +22,14 @@ function checkForm(event){
     var agree = el.nameCheck;
     var date = el.inputDate.value;
     var fail ="";
-    if ( name == "" || komment == "" || nameOwner == "" || agree.checked != true || date == "")
+    if ( name == "" || comment == "" || nameOwner == "" || agree.checked != true || date == "")
         fail = "Please, fill all filds";
     else if(name.length <= 1 || name.length > 20 || nameOwner.length <= 1 || nameOwner.length > 20)
         fail = "Please enter correct name";
     if (fail != "")
         document.getElementById('error').innerHTML = fail;
     else {
-        alert ("All data is correctly filled in!\nName Diamond: "+name+"\nComment: "+komment+"\nImage"+img.value +"\nYour name: "+nameOwner+"\nAge: "+ageValue+"\nDate: "+date);
+        alert ("All data is correctly filled in!\nName Diamond: "+name+"\nComment: "+comment+"\nImage"+img.value +"\nYour name: "+nameOwner+"\nAge: "+ageValue+"\nDate: "+date);
         window.location.reload();
     }
 }
